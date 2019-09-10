be465b81c4ecad3ae5fe11bd9c3f63f94a5d2bf3
Dolores Maria Llidó Escrivá <dllido@MacBook-Pro-de-Dolores.local>
2019-09-10 14:39:39 +0200

    20-21

--- Teoria/Ejercicios/T7/like_button.js
+++ Teoria/Ejercicios/T7/like_button.js
@@ -25,4 +25,8 @@ class LikeButton extends React.Component {
     );
   }
 }
+curl -X POST --data '{"kind":"pyspark","conf":{"spark.master":"yarn","spark.yarn.dist.pyFiles":"/tmp/anotadorImagenRest1.py","queue":"colaLivy"}}' -H "Content-Type":"application/json" localhost:8999/sessions
+curl -X POST --data  '{"kind":"pyspark","pyFiles":["/tmp/anotadorImagenRest0.py"],"conf":{"spark.master":"yarn","spark.queue":"colaLivy"},"name":"AnotaTensorFlow","executorCores":1,"executorMemory":"512m","driverCores":1,"driverMemory":"512m","queue":"colaLivy"}' -H  'Content-Type':'application/json' localhost:8999/sessions
 
+curl -X POST --data '{"code":"a=\"{"id":"1073160116164210689","media":["http://pbs.twimg.com/media/DuShcAcU8AAKxw3.jpg","http://pbs.twimg.com/media/DuShcAcU8AAKxw3.jpg" 
+]}\""}' -H 'Content-Type':'application/json' localhost:8999/sessions/259/statements 
