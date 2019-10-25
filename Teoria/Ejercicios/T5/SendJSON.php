3b33a570d43a711963420e4b46483007732123ee
Dolores Maria Llidó Escrivá <dllido@MacBook-Pro-de-Dolores.local>
2019-10-25 16:58:34 +0200

    public

--- Teoria/Ejercicios/T5/SendJSON.php
+++
@@ -1,9 +0,0 @@
-<?php
-include(dirname(__FILE__)."/../../wp-config.php");
-$pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD);
-header('Content-type: application/json');
-$result = $pdo->prepare("SELECT * FROM A_GrupoCliente");
-$result->execute();
-$datos = $result->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($datos);
-?>
