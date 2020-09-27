c10c6c0649658bbc4516d97db9afae90400c9f94
Dolores Maria Llidó Escrivá <dllido@FgilM.local>
2020-09-27 12:34:33 +0200

    2020

--- P3/P1/P1_M/includes/registro.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$central = "../partials/registerForm.php";
-
-include(dirname(__FILE__)."/../partials/header.php");
-include(dirname(__FILE__)."/../partials/menu.php");
-require_once($central);
-include(dirname(__FILE__)."/../partials/footer.php");
-?>
